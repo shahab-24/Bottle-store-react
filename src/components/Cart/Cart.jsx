687b7fff778488211e8f3a8dbabc5ea143dd97f3
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-10-24 01:03:49 +0600

    proptypes implement for data accuracy

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,19 @@
-
+import PropTypes from "prop-types"; 'prop-types'
 
 const Cart = ({cart}) => {
-    const{img} = cart
+  
+    const{img} = cart;
     return (
         <div>
             <img src={img} alt="" />
         </div>
     );
 };
+
+
+Cart.propTypes = {
+    cart:PropTypes.object.isRequired
+}
+
 
 export default Cart;
